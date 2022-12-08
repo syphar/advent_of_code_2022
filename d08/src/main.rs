fdc62a73171dce780344ef2305f46015d206743d
Denis Cornehl <denis@cornehl.org>
2022-12-08 17:39:06 +0100

    08/2 works

--- d08/src/main.rs
+++ d08/src/main.rs
@@ -2,43 +2,89 @@ fn main() {
     let input = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
 
     println!("day 1: {}", part_1(&input));
-    // println!("day 2: {}", part_2(&input));
+    println!("day 2: {}", part_2(&input));
 }
 
 #[derive(Debug)]
-struct Forest(Vec<Vec<u32>>);
+struct Forest(Vec<Vec<i64>>);
 
 impl Forest {
-    fn size(&self) -> usize {
-        self.0.len()
+    fn size(&self) -> i64 {
+        self.0.len() as i64
     }
 
-    fn tree(&self, x: usize, y: usize) -> u32 {
-        self.0[y][x]
+    fn tree(&self, x: i64, y: i64) -> Option<i64> {
+        self.0.get(y as usize)?.get(x as usize).cloned()
     }
 
-    fn is_visible(&self, x: usize, y: usize) -> bool {
+    fn is_visible(&self, x: i64, y: i64) -> bool {
         if x == 0 || y == 0 || x == (self.size() - 1) || y == (self.size() - 1) {
             return true;
         }
 
-        if (0..x).all(|check_x| self.tree(check_x, y) < self.tree(x, y)) {
+        if (0..x).all(|check_x| self.tree(check_x, y).unwrap() < self.tree(x, y).unwrap()) {
             return true;
         }
 
-        if ((x + 1)..self.size()).all(|check_x| self.tree(check_x, y) < self.tree(x, y)) {
+        if ((x + 1)..self.size())
+            .all(|check_x| self.tree(check_x, y).unwrap() < self.tree(x, y).unwrap())
+        {
             return true;
         }
 
-        if (0..y).all(|check_y| self.tree(x, check_y) < self.tree(x, y)) {
+        if (0..y).all(|check_y| self.tree(x, check_y).unwrap() < self.tree(x, y).unwrap()) {
             return true;
         }
 
-        if ((y + 1)..self.size()).all(|check_y| self.tree(x, check_y) < self.tree(x, y)) {
+        if ((y + 1)..self.size())
+            .all(|check_y| self.tree(x, check_y).unwrap() < self.tree(x, y).unwrap())
+        {
             return true;
         }
 
         false
+    }
+
+    fn calculate_viewing_distance(
+        &self,
+        center_value: i64,
+        check_x: i64,
+        check_y: i64,
+        mut step: impl FnMut(i64, i64) -> (i64, i64),
+    ) -> i64 {
+        let (recurse, return_value) = if let Some(value) = self.tree(check_x, check_y) {
+            if value < center_value {
+                (true, 1)
+            } else {
+                (false, 1)
+            }
+        } else {
+            (false, 0)
+        };
+
+        if recurse {
+            let (new_x, new_y) = step(check_x, check_y);
+
+            return_value + self.calculate_viewing_distance(center_value, new_x, new_y, step)
+        } else {
+            return_value
+        }
+    }
+
+    fn viewing_distance(&self, x: i64, y: i64) -> i64 {
+        if x == 0 || y == 0 || x == (self.size() - 1) || y == (self.size() - 1) {
+            return 0;
+        }
+
+        let center_value = self.tree(x, y).unwrap();
+
+        let l = self.calculate_viewing_distance(center_value, x - 1, y, |x, y| (x - 1, y));
+        let r = self.calculate_viewing_distance(center_value, x + 1, y, |x, y| (x + 1, y));
+
+        let o = self.calculate_viewing_distance(center_value, x, y - 1, |x, y| (x, y - 1));
+        let u = self.calculate_viewing_distance(center_value, x, y + 1, |x, y| (x, y + 1));
+
+        l * r * o * u
     }
 }
 
@@ -53,7 +99,7 @@ fn parse_data(input: &str) -> Forest {
                 }
                 Some(
                     line.chars()
-                        .map(|ch| ch.to_digit(10).expect("unparseable digit"))
+                        .map(|ch| ch.to_digit(10).expect("unparseable digit") as i64)
                         .collect::<Vec<_>>(),
                 )
             })
@@ -76,8 +122,17 @@ fn part_1(input: &str) -> u64 {
     visible_trees
 }
 
-fn part_2(input: &str) -> u64 {
-    todo!();
+fn part_2(input: &str) -> i64 {
+    let forest = parse_data(input);
+
+    let mut max_viewing_distance = 0;
+
+    for x in 0..forest.size() {
+        for y in 0..forest.size() {
+            max_viewing_distance = max_viewing_distance.max(forest.viewing_distance(x, y))
+        }
+    }
+    max_viewing_distance
 }
 
 #[cfg(test)]
@@ -102,10 +157,18 @@ mod tests {
     #[test_case(1, 3, 3, false; "8")]
     #[test_case(2, 3, 5, true; "9")]
     #[test_case(3, 3, 4, false; "10")]
-    fn test_hidden_inner(x: usize, y: usize, value: u32, expected: bool) {
+    fn test_hidden_inner(x: i64, y: i64, value: i64, expected: bool) {
         let forest = dbg!(parse_data(TEST_INPUT));
-        assert_eq!(forest.tree(x, y), value);
+        assert_eq!(forest.tree(x, y).unwrap(), value);
         assert_eq!(forest.is_visible(x, y), expected);
+    }
+
+    #[test_case(2, 1, 5, 4; "1")]
+    #[test_case(2, 3, 5, 8; "2")]
+    fn test_viewing_distance(x: i64, y: i64, value: i64, expected: i64) {
+        let forest = dbg!(parse_data(TEST_INPUT));
+        assert_eq!(forest.tree(x, y).unwrap(), value);
+        assert_eq!(forest.viewing_distance(x, y), expected);
     }
 
     #[test]
@@ -115,6 +178,6 @@ mod tests {
 
     #[test]
     fn test_2() {
-        assert_eq!(part_1(TEST_INPUT), 8)
+        assert_eq!(part_2(TEST_INPUT), 8)
     }
 }
